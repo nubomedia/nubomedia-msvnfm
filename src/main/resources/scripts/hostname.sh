480d080899da1e5d23239b3d306d95950557f321
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2016-01-28 02:22:32 +0100

    updated script

--- src/main/resources/scripts/hostname.sh
+++ src/main/resources/scripts/hostname.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -v
 echo $HOSTNAME > /etc/hostname
-HOSTNAME=$(cat /etc/hostname)
+#HOSTNAME=$(cat /etc/hostname)
 #sudo sed -i "s/\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*[ \t]*localhost\)/\1 $HOSTNAME/" /etc/hosts
 
-INSTANCE_NAME=$(curl http://169.254.169.254/latest/meta-data/hostname)
-IFS='.' read -ra array <<< "$INSTANCE_NAME"
-instance_name_simple=${array[0]}
+#INSTANCE_NAME=$(curl http://169.254.169.254/latest/meta-data/hostname)
+#IFS='.' read -ra array <<< "$INSTANCE_NAME"
+#instance_name_simple=${array[0]}
 
-sed -i " 1 s/.*/& $instance_name_simple/" /etc/hosts  
+sed -i " 1 s/.*/& $HOSTNAME/" /etc/hosts  
