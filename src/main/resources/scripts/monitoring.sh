44d6909c0b46708d41ac98f709b9412bab811bc2
michael.pauls <michael.pauls@mailbox.tu-berlin.de>
2016-01-29 16:37:31 +0100

    updated scripts

--- src/main/resources/scripts/monitoring.sh
+++ src/main/resources/scripts/monitoring.sh
@@ -4,7 +4,6 @@
 #MONITORING_URL='172.22.28.70'
 #sed -i -e "s/172.22.2.70/$MONITORING_URL/g" /etc/collectd/collectd.conf
 #/etc/init.d/collectd restart
-
-HOSTNAME_MONITORING="HOSTNAMEMONITORING"
+#HOSTNAME_MONITORING="HOSTNAMEMONITORING"
 sed -i -e "s/HOSTNAMEMONITORING/$HOSTNAME/g" /etc/collectd/collectd.conf 
 service collectd restart
