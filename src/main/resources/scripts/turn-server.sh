9da266bdc3119e034779933ecfe701dc8ca02ab0
michael.pauls <michael.pauls@mailbox.tu-berlin.de>
2016-02-04 18:50:37 +0100

    Changed default pattern for replacing STUN/TURN-Server
    Since the default values were updated on the image these changes
    were also applied on the script that replaces the default values
    with user requested ones.

--- src/main/resources/scripts/turn-server.sh
+++ src/main/resources/scripts/turn-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ "$TURN_SERVER_ACTIVATE" = "true" ]; then
-    sed -i -e "s/turnURL=nubomedia:nub0m3d1a:80.96.122.61/turnURL=$TURN_SERVER_USERNAME:$TURN_SERVER_PASSWORD:$TURN_SERVER_URL/g" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+    sed -i -e "s/turnURL=nubomedia:nub0m3d1a:80.96.122.61:3478/turnURL=$TURN_SERVER_USERNAME:$TURN_SERVER_PASSWORD:$TURN_SERVER_URL/g" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
