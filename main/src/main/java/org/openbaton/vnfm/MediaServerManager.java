1989ff7f8b8aed0b6ab6e05a66e5392ada9ef1ff
michael.pauls <michael.pauls@mailbox.tu-berlin.de>
2016-04-20 17:13:58 +0200

    Improved heartbeat processing when restarting EMM

--- main/src/main/java/org/openbaton/vnfm/MediaServerManager.java
+++ main/src/main/java/org/openbaton/vnfm/MediaServerManager.java
@@ -447,9 +447,12 @@ public class MediaServerManager extends AbstractVnfmSpringAmqp implements Applic
         }
         log.info("Set number of missed heartbeats to 0 for all applications.");
         for (Application app : applicationManagement.query()) {
-            app.setMissedHeartbeats(0);
-            app.setHeartbeat(new Date());
-            log.debug("Set missed heartbeats to 0 for application " + app.getId());
+            try {
+                applicationManagement.heartbeat(app.getVnfr_id(), app.getId());
+            } catch (NotFoundException e) {
+                log.error(e.getMessage(), e);
+            }
+            log.debug("Set missed heartbeats to 0 for application " + app);
         }
         if (applicationProperties.getHeartbeat().isActivate()) {
             applicationManagement.startHeartbeatCheck();
